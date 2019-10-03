ab745c238d0b2fd3c585d00008d117b7c76fc7a7
NKalavros <39969236+NKalavros@users.noreply.github.com>
2019-10-03 05:48:56 +0300

    Update mirlab_analysis_tcga_data.R

--- mirlab_analysis_tcga_data.R
+++ mirlab_analysis_tcga_data.R
@@ -2,48 +2,62 @@ mirlab_tcga_analysis <- function(mrna_matrix,microrna_matrix,cancer = 1,mrna_kee
   #Packages needed
   require("miRLAB")
   require("edgeR")
+  mrna_matrix <- mrnacountmatrix
+  microrna_matrix <- mircountmatrix
   #Get common patients
-  common_patients <- intersect(substring(colnames(mrna_matrix),1,15),substring(colnames(microrna_matrix),1,15))
   #Keep cancer or normals only, by default keeps only samples of cancer patients
-  if(cancer == 1){
-    common_patients <- common_patients[substring(common_patients,14,15) != "11"]
-  }
-  else{
-    common_patients <- common_patients[substring(common_patients,14,15) == "11"]
-  }
   #Subset matrices
-  common_patients_mrna <- mrna_matrix[,common_patients]
-  common_patients_microrna <- mrna_matrix[,common_patients]
+  if(cancer == 1){
+    mrna_matrix <- mrna_matrix[,substring(colnames(mrna_matrix),14,15) != "11"]
+    microrna_matrix <- microrna_matrix[,substring(colnames(microrna_matrix),14,15) != "11"]
+  }else{
+    microrna_matrix <- microrna_matrix[,substring(microrna_matrix,14,15) == "11"]
+    mrna_matrix <- mrna_matrix[,substring(mrna_matrix,14,15) == "11"]
+  }
+  mir_in_mrna <- match(substring(colnames(mrna_matrix),1,15),substring(colnames(microrna_matrix),1,15))
+  mir_in_mrna <- mir_in_mrna[!is.na(mir_in_mrna)]
+  microrna_matrix <- microrna_matrix[,mir_in_mrna]
+  mrna_in_mir <- match(substring(colnames(microrna_matrix),1,15),substring(colnames(mrna_matrix),1,15))
+  mrna_in_mir <- mrna_in_mir[!is.na(mrna_in_mir)]
+  mrna_matrix <- mrna_matrix[,mrna_in_mir]
+  if(!all(substring(colnames(mrna_matrix),1,15) == substring(colnames(microrna_matrix),1,15))){
+    print("Could not match all patients")
+    return(1)
+  }
   #Perform TMM normalization
   #Turn into DGEList
-  common_patients_mrna <- DGEList(common_patients_mrna)
-  common_patients_microrna <- DGEList(common_patients_microrna)
-  #Normalize
-  common_patients_mrna <- calcNormFactors(common_patients_mrna, method = "TMM")
-  common_patients_microrna <- calcNormFactors(common_patients_microrna, method = "TMM")
+  mrna_matrix <- DGEList(mrna_matrix)
+  microrna_matrix <- DGEList(microrna_matrix)
+  #Normalize, obtain counts
+  mrna_matrix <- calcNormFactors(mrna_matrix, method = "TMM")
+  mrna_matrix <- mrna_matrix$counts
+  microrna_matrix <- calcNormFactors(microrna_matrix, method = "TMM")
+  microrna_matrix <- microrna_matrix$counts
   print("Normalization for both datasets complete")
   #In the interest of time, use only the 100 most highly expressed miRs
   #and the 2000 most highly expressed mRNA
   #Compute row averages
-  average_expr_mrna <- rowSums(common_patients_mrna)/ncol(common_patients_mrna)
-  average_expr_microrna <- rowSums(common_patients_microrna)/ncol(common_patients_microrna)
+  average_expr_mrna <- rowSums(mrna_matrix)/ncol(mrna_matrix)
+  average_expr_microrna <- rowSums(microrna_matrix)/ncol(microrna_matrix)
   #Get their order
-  order_mrna <- order(average_expr_mrna)
-  order_microrna <- order(average_expr_microrna)
-  #Get only the top expressed ones, up to K and L (set in function definition)
-  if(mrna_keep < nrow(common_patients_mrna)){
-    common_patients_mrna <- common_patients_mrna[order_mrna[1:mrna_keep],]
+  order_mrna <- order(average_expr_mrna,decreasing = TRUE)
+  order_microrna <- order(average_expr_microrna, decreasing = TRUE)
+    #Get only the top expressed ones, up to K and L (set in function definition)
+  if(mrna_keep < nrow(mrna_matrix)){
+    mrna_matrix <- mrna_matrix[order_mrna[1:mrna_keep],]
   }
-  if(microrna_keep < nrow(common_patients_microrna)){
-    common_patients_microrna <- common_patients_microrna[order_microrna[1:microrna_keep],]
+  if(microrna_keep < nrow(microrna_matrix)){
+    microrna_matrix <- microrna_matrix[order_microrna[1:microrna_keep],]
   }
+  print(paste("Completed subsetting of matrices. Final dimensions for mRNA are:",paste(dim(mrna_matrix), collapse = "x"),"and for miR are:",paste(dim(microrna_matrix),collapse="x")))
   #Set cause and effect
-  cause <- 1:nrow(common_patients_microrna)
-  effect <- cause+1:(cause+1 + nrow(common_patients_mrna))
+  cause <- 1:nrow(microrna_matrix)
+  effect <- (cause[length(cause)]+1):(cause[length(cause)] + nrow(mrna_matrix))
   #Create cause and effect matrix
-  dataset <- rbind(common_patients_mrna,common_patients_microrna)
+  dataset <- rbind(microrna_matrix,mrna_matrix)
+  write.csv(dataset,"dataset.csv",row.names = FALSE,quote = FALSE)
   #predict miRNA targets using Mutual Information
-  mi=MI(dataset, cause, effect)
+  mi = MI("dataset.csv", cause, effect)
   print("Mutual information analysis complete")
   #predict miRNA targets using causal inference
   ida=IDA(dataset, cause, effect, "stable", 0.01)
